fe4ed01d5dc62aeb550d3741386c64eec02482af
RedoRojo <cristhiabnvictorrm@gmaill.com>
2024-12-10 12:36:07 -0400

    inmutable

---
+++ joselito/nose.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define all(a) (a).begin(), (a).end()
+#define vi vector<int>
+#define int long long int
+
+using namespace std;
+
+int32_t main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n; 
+    cin>>n; 
+
+    return 0;
+}
